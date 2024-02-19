815433ec00b2590aea0e8bf3bcaee39ac6b332a9
mr_yoso <rj0gayo@gmail.com>
2024-02-19 12:38:51 -0500

    finished the Counter.php in app\models

---
+++ app/models/Counter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\models;
+
+class Counter
+{
+
+    public $count;
+
+    public function __construct($count)
+    {
+        $filename = 'resources/counter.txt';
+        if (file_exists($filename)) {
+            $handle = fopen($filename,'r');
+            flock($handle, LOCK_EX);
+            $this->count = json_decode(fread($handle, filesize($filename)))->count;
+            fclose($handle);
+        } else {
+            $this->count = 0;
+        }
+    }
+
+    public function increment() {
+        $this->count++;
+    }
+
+    public function write() {
+        $filename = 'resources/counter.txt';
+        $data = json_encode(['count' => $this->count]);
+        $handle = fopen($filename,'w');
+        flock($handle, LOCK_EX);
+        fwrite($handle, $data);
+        fclose($handle);
+    }
+
+    public function __toString() {
+        return json_encode($this);
+    }
+}
